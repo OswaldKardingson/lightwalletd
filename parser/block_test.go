8815d6e45e20f7645388f4a0c8f2058f6c73d31b
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2024-12-08 03:39:51 +0200

    Updated test with Merkle Frontiers
    Still requires block data

--- parser/block_test.go
+++ parser/block_test.go
@@ -1,7 +1,8 @@
 // Copyright (c) 2019-2020 The Zcash developers
-// Copyright (c) 2019-2021 Pirate Chain developers
+// Copyright (c) 2019-2024 Pirate Chain developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php .
+
 package parser
 
 import (
@@ -9,12 +10,13 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"testing"
 
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 
-	protobuf "github.com/golang/protobuf/proto"
+	protobuf "google.golang.org/protobuf/proto"
 )
 
 func TestCompactBlocks(t *testing.T) {
@@ -27,7 +29,7 @@ func TestCompactBlocks(t *testing.T) {
 	}
 	var compactTests []compactTest
 
-	blockJSON, err := ioutil.ReadFile("../testdata/compact_blocks.json")
+	blockJSON, err := io.ReadFile("../testdata/compact_blocks.json")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -76,5 +78,73 @@ func TestCompactBlocks(t *testing.T) {
 			break
 		}
 	}
+}
 
+// New tests for MerkleFrontier integration
+
+func getValidBlockData() []byte {
+	// Mock valid block data
+	return []byte{
+		/* Add valid block bytes here */
+	}
+}
+
+func getEmptyBlockData() []byte {
+	// Mock empty block data
+	return []byte{
+		/* Add empty block bytes here */
+	}
+}
+
+func getCorruptBlockData() []byte {
+	// Mock corrupt block data
+	return []byte{
+		/* Add corrupt block bytes here */
+	}
+}
+
+func TestParseFromSliceWithMerkleFrontier(t *testing.T) {
+	// Case 1: Valid block with multiple transactions
+	block := NewBlock()
+	data := getValidBlockData() // Mock valid block data
+	_, err := block.ParseFromSlice(data)
+	assert.NoError(t, err, "Parsing a valid block should not produce an error")
+	assert.NotNil(t, block.MerkleFrontier, "MerkleFrontier should be initialized")
+	assert.NotEmpty(t, block.MerkleFrontier.GetRoot(), "Merkle root should not be empty for valid block")
+
+	// Case 2: Empty block
+	block = NewBlock()
+	data = getEmptyBlockData() // Mock empty block data
+	_, err = block.ParseFromSlice(data)
+	assert.NoError(t, err, "Parsing an empty block should not produce an error")
+	assert.Empty(t, block.MerkleFrontier.GetRoot(), "Merkle root should be empty for an empty block")
+
+	// Case 3: Invalid block with corrupt transaction
+	block = NewBlock()
+	data = getCorruptBlockData() // Mock corrupt block data
+	_, err = block.ParseFromSlice(data)
+	assert.Error(t, err, "Parsing a block with corrupt transaction should produce an error")
+}
+
+func TestMerkleFrontierQueries(t *testing.T) {
+	// Mock a block with multiple transactions
+	block := NewBlock()
+	data := getValidBlockData() // Mock valid block data
+	_, err := block.ParseFromSlice(data)
+	assert.NoError(t, err)
+
+	// Case 1: Generate Merkle root
+	root, err := block.GetMerkleRoot()
+	assert.NoError(t, err, "Getting Merkle root should not produce an error")
+	assert.NotEmpty(t, root, "Merkle root should not be empty")
+
+	// Case 2: Generate proof for valid transaction index
+	proof, err := block.MerkleFrontier.GenerateProof(0) // Proof for first transaction
+	assert.NoError(t, err, "Generating proof for valid transaction should not produce an error")
+	assert.NotNil(t, proof, "Proof should not be nil")
+
+	// Case 3: Generate proof for invalid transaction index
+	proof, err = block.MerkleFrontier.GenerateProof(999) // Out-of-bounds index
+	assert.Error(t, err, "Generating proof for invalid index should produce an error")
+	assert.Nil(t, proof, "Proof for invalid index should be nil")
 }
